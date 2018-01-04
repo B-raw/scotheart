3516fa241aa2e752b4902cdca8860dfdd93fdbb8
B-raw <michaelbarry1989@hotmail.com>
2018-01-04 17:41:47 +0000

    remove unneeded pages + routes

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -19,26 +19,6 @@ FlowRouter.route('/about', {
   }
 });
 
-FlowRouter.route('/signup', {
-  name: 'signup',
-  action() {
-    if(Meteor.userId()) {
-      FlowRouter.go('home');
-    }
-    BlazeLayout.render("mainLayout", { content: "Signup" });
-  }
-});
-
-FlowRouter.route('/login', {
-  name: 'login',
-  action() {
-    if(Meteor.userId()) {
-      FlowRouter.go('home');
-    }
-    BlazeLayout.render("mainLayout", { content: "Login" });
-  }
-});
-
 FlowRouter.route('/terms-conditions', {
   name: 'terms-conditions',
   action() {
@@ -50,27 +30,6 @@ FlowRouter.route('/privacy-policy', {
   name: 'privacy-policy',
   action() {
     BlazeLayout.render("mainLayout", { content: "PrivacyPolicy" });
-  }
-});
-
-FlowRouter.route('/pathway', {
-  name: 'pathway',
-  action() {
-    BlazeLayout.render("mainLayout", { content: "Pathway" });
-  }
-});
-
-FlowRouter.route('/funding', {
-  name: 'funding',
-  action() {
-    BlazeLayout.render("mainLayout", { content: "Funding" });
-  }
-});
-
-FlowRouter.route('/definition', {
-  name: 'definition',
-  action() {
-    BlazeLayout.render("mainLayout", { content: "Definition" });
   }
 });
 
--- imports/ui/layouts/mainLayout.js
+++ imports/ui/layouts/mainLayout.js
@@ -6,14 +6,8 @@ import './navbar.js';
 
 import '../frontpage/frontpage.js';
 import '../about/about.js';
-import '../signup/signup.js';
-import '../login/login.js';
 import '../privacyPolicy/privacyPolicy.js'
 import '../termsConditions/termsConditions.js'
-import '../pathway/pathway.js';
-import '../viewPathwayButton/viewPathwayButton.js'
-import '../funding/funding.js';
-import '../definition/definition.js';
 import '../contact/contact.js';
 import '../calculator/calculator.js';
 import '../scotheartInformationPanel/scotheartInformationPanel.js'
