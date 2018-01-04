6e62be1198c93f8fb378eb5a748b04a6752917ee
B-raw <michaelbarry1989@hotmail.com>
2018-01-04 18:03:19 +0000

    remove bug from data toggle navbar

--- imports/ui/layouts/navbar.js
+++ imports/ui/layouts/navbar.js
@@ -4,12 +4,7 @@ import { FlowRouter } from "meteor/kadira:flow-router";
 import './navbar.html';
 
 Template.Navbar.events({
-  'click #logout'(event) {
-    event.preventDefault();
-    Meteor.logout();
-
-    FlowRouter.go('home');
-    }
+  
 })
 
 Template.Navbar.helpers({
